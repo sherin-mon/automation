8e7d153053a0868b6c3b5eaedbd36c3243d4bb12
sherin-mon <153709914+sherin-mon@users.noreply.github.com>
2024-12-31 11:18:07 +0530

    Delete terraform directory

--- terraform/modules/cloudflare/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "cloudfront_url" {
-  description = "The CloudFront URL to use as the value of the CNAME record."
-  type        = string
-}
-
--- terraform/modules/s3/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "cloudfront_arn" {
-  description = "ARN of the CloudFront distribution"
-  type        = string
-}
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-}
